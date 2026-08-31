a1c374f5543ef2505aefa0058e5687cb1d77a3b5
jamaluddinkhanvgcchjff-jpg <jamaluddinkhanvgcchjff@gmail.com>
2026-08-31 22:36:23 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
-const express=require("express"),http=require("http"),{Server}=require("socket.io"),path=require("path");
-const app=express(),server=http.createServer(app),io=new Server(server);
-app.use(express.static(path.join(__dirname,"public")));
-io.on("connection",socket=>{
- socket.on("join-room",room=>{socket.join(room);socket.to(room).emit("peer-joined");});
- socket.on("signal",({room,data})=>socket.to(room).emit("signal",data));
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(__dirname));
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (room) => {
+    socket.join(room);
+    socket.to(room).emit("peer-joined");
+  });
+
+  socket.on("signal", ({ room, data }) => {
+    socket.to(room).emit("signal", data);
+  });
 });
-const PORT=process.env.PORT||3000;server.listen(PORT,()=>console.log("WorkMobile running on "+PORT));
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log("WorkMobile running on port " + PORT));
